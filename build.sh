052b7128979b191904cff9deb787288aac3c48e7
Cesar1xd1 <cesar1xd1@gmail.com>
2025-03-26 09:23:28 -0600

    agregue php y composer

--- build.sh
+++ build.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-# Instalar Composer
-curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+# Instalar PHP (si no está instalado)
+if ! command -v php &> /dev/null
+then
+    echo "PHP no está instalado. Instalando..."
+    apt-get update && apt-get install -y php php-cli php-fpm php-mbstring php-xml php-mysql php-curl
+fi
 
-# Instalar las dependencias de Composer
+# Instalar Composer (si no está instalado)
+if ! command -v composer &> /dev/null
+then
+    echo "Composer no está instalado. Instalando..."
+    curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+fi
+
+# Instalar dependencias de Composer
 composer install --no-dev --optimize-autoloader
