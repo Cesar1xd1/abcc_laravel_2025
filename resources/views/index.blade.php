50def68b49b27dc61d53c00ad11efd85e67aa423
Cesar1xd1 <cesar1xd1@gmail.com>
2025-04-13 22:32:27 -0600

    Se añadio la linea de link de bootstrap

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,7 @@
 @section('title', 'Alumnos')
 
 @section('content_header')
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <h1 class="text-center">SERVICIOS ESCOLARES</h1>
 @endsection
 
